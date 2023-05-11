4b7fb700b418c2a80c455e3ace798abd35e153e2
KeithLin7024 <keithlin724@gmail.com>
2023-05-11 23:52:11 +0800

    update use GPU to run

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,8 @@ package main;
 
 public class Main {
 	public static void main(String[] args) {
+		System.setProperty("sun.java2d.opengl", "true");
+
 		Game game = new Game();
 		game.runGame();
 	}
